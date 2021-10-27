a14b75d8e3b5ae5905d19a492b4612fc921a3a3c
sardoraminov <oddiydasturchi@gmail.com>
2021-10-27 20:09:31 +0500

    Commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <home></home>
 </template>
 
+<script>
+import Home from "./views/Home.vue";
+
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
+
 <style>
+body {
+  background-attachment: fixed;
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
 #app {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,179 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home wh-app">
+    <div class="titles">
+      <h1>Weather App <img src="../assets/weather.png" alt="Weather" /></h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste,
+        consequatur
+      </p>
+    </div>
+    <div class="weathers">
+      <input
+        @keypress="fetchWeather"
+        type="text"
+        v-model="query"
+        placeholder="Enter your city..."
+        autocomplete="off"
+      />
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <h1 class="city">{{ weather.name }} {{ weather.sys.country }}</h1>
+        <h5 class="today">Wednesday 27 October 2021</h5>
+        <div class="weathers-temp">
+          <h1 class="temp">{{ Math.round(weather.main.temp) }}°C</h1>
+          <p class="status">{{ weather.weather[0].main }}</p>
+        </div>
+      </div>
+
+      <div class="weather-wrap" v-else>
+        <h1>Please, enter valid city</h1>
+      </div>
+
+      <footer>
+        Developed by<a href="https://instagram.com/s_aminoff">SARDOR</a>
+      </footer>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+
+  data() {
+    return {
+      base_url: `https://api.openweathermap.org/data/2.5/`,
+      api_key: "ff739f4aecaccaa3e01cd2590f59d60e",
+      query: "",
+      weather: {},
+      errMsg: "",
+    };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key === "Enter") {
+        fetch(
+          `${this.base_url}weather?q=${this.query}&appid=${this.api_key}&units=metric`
+        )
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            this.setResults(data);
+          });
+      }
+    },
+
+    setResults(result) {
+      this.weather = result;
+    },
+  },
+};
 </script>
+
+
+<style scoped>
+.home {
+  margin-top: 40px;
+  color: #fff;
+}
+
+.titles {
+  padding: 0 15px;
+}
+
+.weather-wrap {
+  margin-top: 10px;
+}
+
+.titles h1 {
+  font-family: "Montserrat", sans-serif;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  text-shadow: 2px 2px rgba(58, 58, 58, 0.685);
+}
+
+.titles h1 img {
+  margin-left: 15px;
+}
+
+.titles p {
+  margin-top: 10px;
+  font-family: "Quicksand", sans-serif;
+  color: rgba(255, 255, 255, 0.863);
+  letter-spacing: 1px;
+}
+
+.weathers {
+  margin-top: 20px;
+}
+
+.weathers input {
+  background-color: rgba(255, 255, 255, 0.15);
+  backdrop-filter: blur(15px);
+  border: none;
+  outline: none;
+  padding: 10px 15px;
+  color: #fff;
+  font-family: "Quicksand", sans-serif;
+  border: 2px solid rgba(255, 255, 255, 0.226);
+  border-radius: 5px;
+  transition: all 0.4s ease;
+}
+
+.weathers input:focus {
+  margin-bottom: 10px;
+  border-color: #fff;
+}
+
+.weathers input::placeholder {
+  color: rgba(255, 255, 255, 0.61);
+}
+
+.weathers .city {
+  text-shadow: 2px 2px rgba(58, 58, 58, 0.685);
+
+  margin-top: 15px;
+}
+
+.weathers .today {
+  font-style: italic;
+  font-family: "Quicksand", sans-serif;
+  font-weight: 400;
+  color: rgba(250, 250, 250, 0.623);
+}
+
+.weathers .weathers-temp {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.weathers-temp h1 {
+  background-color: rgba(255, 255, 255, 0.15);
+  backdrop-filter: blur(15px);
+  padding: 10px;
+  font-size: 53px;
+  text-shadow: 2px 2px rgba(58, 58, 58, 0.685);
+  border-radius: 5px;
+}
+
+.weathers-temp p {
+  margin-top: 10px;
+}
+
+footer {
+  margin-top: 100px;
+  color: rgba(250, 250, 250, 0.623);
+  font-family: "Quicksand", sans-serif;
+}
+
+footer a {
+  text-decoration: none;
+  color: rgba(250, 250, 250, 0.623);
+  margin-left: 5px;
+}
+</style>
